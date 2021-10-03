759c53e8d806b6bd190671f6505f199eccbebdea
tomit4 <mosssap@gmail.com>
2021-10-03 04:40:30 -0700

    added more functionality from yt swildermuth tutorial

--- vue.js
+++ vue.js
@@ -8,20 +8,71 @@ function Counter(props) {
     }
 }
 
-function ListEm(props) {
-    return {
-        $template: '#list-template',
-        listItem: props.someKind,
-        li() {
-            console.log(this.listItem)
-        },
-    }
-}
-
 PetiteVue.createApp({
     Counter,
 }).mount()
 
 PetiteVue.createApp({
-    ListEm,
-}).mount()
+    message: {
+        fullName: '',
+        email: '',
+        subject: '',
+        body: '',
+    },
+    status: '',
+    errors: {
+        valid: false,
+        fullName: '',
+        email: '',
+        subject: '',
+        body: '',
+    },
+    send() {
+        if (this.validate()) {
+            this.status = `Sent to ${this.message.fullName}`
+        }
+    },
+    validate() {
+        let valid = true
+
+        const { fullName, email, subject, body } = this.message
+
+        if (!fullName) {
+            this.errors.fullName = 'Full name is required'
+            valid = false
+        } else if (fullName.length < 5) {
+            this.errors.fullName = 'Full name must be at least 5 characters'
+            valid = false
+        } else this.errors.fullName = ''
+
+        if (!email) {
+            this.errors.email = 'Email is required'
+            valid = false
+        } else if (!email.match(/^[^@]+@[^@]+\.[^@]+$/)) {
+            this.errors.email = 'Must be a valid email address'
+            valid = false
+        } else this.errors.email = ''
+
+        if (!subject) {
+            this.errors.subject = 'Subject is required'
+            valid = false
+        } else if (subject.length < 5) {
+            this.errors.subject = 'Subject must be at least 5 characters'
+            valid = false
+        } else this.errors.subject = ''
+
+        if (!body) {
+            this.errors.body = 'Body is required'
+            valid = false
+        } else if (body.length < 5) {
+            this.errors.body = 'Body must be at least 5 characters'
+            valid = false
+        } else if (body.length > 500) {
+            this.errors.body = 'Body must be less than 500 characters'
+            valid = false
+        } else this.errors.body = ''
+
+        this.errors.valid = valid
+        return valid
+    },
+}).mount('#theForm')
